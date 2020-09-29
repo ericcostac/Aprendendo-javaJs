3c45f07531caf242eec63020493eb4c8cd419115
Eric Alberto Costa e Costa <71726316+ericcostac@users.noreply.github.com>
2020-09-28 22:17:28 -0300

    Autentificador de idade

--- project_js/js/main.js
+++ project_js/js/main.js
@@ -1,3 +1,11 @@
-alert("Seja Bem vindo!");
+/*alert("Seja Bem vindo!");
 var fruta = {nome: "maça", cor: "vermelha"}
 console.log(fruta);
+*/
+
+var idade = prompt("Qual a sua idade?");
+if (idade >=18){
+    alert("Maior de idade, acesso permitido!")
+}else{
+    alert("Menor de idade, acesso negado")
+}
